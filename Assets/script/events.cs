ddf8fbc170df9a9d355e3a16aa9e1c72347a85f1
Nithin-3 <141541101+Nithin-3@users.noreply.github.com>
2024-04-29 23:17:30 +0530

    tgt

--- Assets/script/events.cs
+++ Assets/script/events.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class events : MonoBehaviour
 {
     public void TrigerPausePlay(bool pause)
     {
         Time.timeScale = pause ? 0 : 1.0f ;
+    }
+    public void play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void quit()
+    {
+        Application.Quit();
     }
 }
