db4e2fac4609a888e3e17e84ed7476fc3cff0693
FIRESTROM GT <gohari78g@gmail.com>
2024-04-29 23:17:56 +0530

    some improvements

--- Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
+++ Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -21,6 +21,7 @@ public class FirstPersonMovement : MonoBehaviour
     {
         // Get the rigidbody on this.
         rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(transform.up * 250f);
     }
 
     void FixedUpdate()
