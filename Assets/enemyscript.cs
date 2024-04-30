f05fbee01f4c11974cac0f149e738a7552261b21
FIRESTROM GT <gohari78g@gmail.com>
2024-05-01 03:21:58 +0530

    hdbjf

--- Assets/enemyscript.cs
+++ Assets/enemyscript.cs
@@ -1,29 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemyscript : MonoBehaviour
 {
     public GameObject enem1;
+    private GameObject player;
     private Animator animator;
+    private NavMeshAgent agent;
+    public static bool isEnter = false;
     void Awake()
     {
         animator = enem1.GetComponent<Animator>();
+        agent = enem1.GetComponent<NavMeshAgent>();
     }
-    
-    private void OnCollisionEnter(Collision collision)
+
+    void Update()
     {
-        if (collision.gameObject.CompareTag("Player"))
+        try
         {
+        player = GameObject.Find("Player(Clone)");
+
+        }
+        catch {
+            
+        }
+        if (player != null)
+        {
+            agent.destination = player.transform.position;
+            Debug.Log(Vector3.Distance(transform.position, player.transform.position));
+           
+            if (Vector3.Distance(transform.position, player.transform.position) < 10)
+            {
+                transform.LookAt(player.transform.position);
+            }
+            
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("GHDGJ");
+            isEnter = true;
             animator.SetBool("isRun", true);
         }
     }
 
-
-    private void OnCollisionExit(Collision collision)
+    private void OnTriggerExit(Collider other)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
+            isEnter = false;
             animator.SetBool("isRun", false);
         }
     }
