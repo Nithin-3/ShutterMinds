681275539cd695b11150718e87de9abbbdf91384
FIRESTROM GT <gohari78g@gmail.com>
2024-05-01 00:49:15 +0530

    hjkhj

--- Assets/enemyscript.cs
+++ Assets/enemyscript.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class enemyscript : MonoBehaviour
-{ 
+{
+    public GameObject enem1;
     private Animator animator;
     void Awake()
     {
-        animator = GetComponent<Animator>();
+        animator = enem1.GetComponent<Animator>();
     }
     
     private void OnCollisionEnter(Collision collision)
