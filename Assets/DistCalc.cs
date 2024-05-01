6308446acf17ecf0c4b69e528c646c8293d58023
FIRESTROM GT <gohari78g@gmail.com>
2024-05-01 11:57:59 +0530

    dshds

--- Assets/DistCalc.cs
+++ Assets/DistCalc.cs
@@ -6,7 +6,7 @@ using TMPro;
 
 public class DistCalc : MonoBehaviour
 {
-    private TMP_Text distanceTxt;
+    public TMP_Text distanceTxt;
 
     //private NavMeshAgent agent1;
     public float distance1;
@@ -16,7 +16,7 @@ public class DistCalc : MonoBehaviour
     public float distance5;
     public float distance6;
 
-    private GameObject player;
+    private GameObject player1;
 
     private GameObject leg1;
     private GameObject leg2;
@@ -26,29 +26,42 @@ public class DistCalc : MonoBehaviour
     private GameObject head;
     // Start is called before the first frame update
     void Start()
-    {;
+    {
+        
         leg1 = GameObject.Find("leftleg");
         leg2 = GameObject.Find("rightleg");
         hand1 = GameObject.Find("lefthand");
         hand2 = GameObject.Find("righthand");
         body = GameObject.Find("body");
         head = GameObject.Find("head");
-
-        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        distance1 = Vector3.Distance(player.transform.position, leg1.transform.position);
-        distance2 = Vector3.Distance(player.transform.position, leg2.transform.position);
-        distance3 = Vector3.Distance(player.transform.position, hand1.transform.position);
-        distance4 = Vector3.Distance(player.transform.position, hand2.transform.position);
-        distance5 = Vector3.Distance(player.transform.position, body.transform.position);
-        distance6 = Vector3.Distance(player.transform.position, head.transform.position);
+        try
+        {
+            player1 = GameObject.Find("Player(Clone)");
 
-        float smalldist = Mathf.Min(distance1, distance2,distance3,distance4,distance5,distance6);
+        }
+        catch
+        {
 
-        distanceTxt.text = "BodyPart Distance " + smalldist;
+        }
+
+        if(player1 != null)
+        {
+            distance1 = Vector3.Distance(player1.transform.position, leg1.transform.position);
+            distance2 = Vector3.Distance(player1.transform.position, leg2.transform.position);
+            distance3 = Vector3.Distance(player1.transform.position, hand1.transform.position);
+            distance4 = Vector3.Distance(player1.transform.position, hand2.transform.position);
+            distance5 = Vector3.Distance(player1.transform.position, body.transform.position);
+            distance6 = Vector3.Distance(player1.transform.position, head.transform.position);
+
+            float smalldist = Mathf.Min(distance1, distance2, distance3, distance4, distance5, distance6);
+
+            distanceTxt.text = "BodyPart Distance " + smalldist;
+        }
+        
     }
 }
